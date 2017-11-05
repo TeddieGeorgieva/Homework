7efe1ee594c9d8b05c46a8fc9033420ab5767944
TeddieGeorgieva <32646436+TeddieGeorgieva@users.noreply.github.com>
2017-11-05 13:57:41 +0200

    Add files via upload

---
+++ Task5.cpp
@@ -0,0 +1,32 @@
+// Task5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a, b, c;
+	int count = 0;
+
+	for (int i = 100; i<1000; i++)
+	{
+		    a = i / 100 % 10,
+			b = i / 10 % 10,
+			c = i % 10;
+
+		if ( a == b || a ==c || b == c){
+			continue;
+	}
+		cout << i << " " << endl;
+		count++;
+} 
+	cout << count << endl;
+
+	system("PAUSE");
+    return 0;
+}
+
